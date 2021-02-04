133f1447faf628d13933cf85696f5db338183c3f
Rainer Sawitzki <training@rainer-sawitzki.de>
2021-02-04 15:31:00 +0100

    add simple producer and consumer

---
+++ org.javacream.training.kafka/src/main/java/org/javacream/training/kafka/Configuration.java
@@ -0,0 +1,7 @@
+package org.javacream.training.kafka;
+
+public class Configuration {
+
+	public static final String HOST = "localhost";
+	public static final String BOOTSTRAP_SERVERS = HOST + ":9092";
+}
---
+++ org.javacream.training.kafka/src/main/java/org/javacream/training/kafka/topic/simple/SimpleProducer.java
@@ -0,0 +1,31 @@
+package org.javacream.training.kafka.topic.simple;
+
+import java.util.Properties;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.javacream.training.kafka.Configuration;
+
+public class SimpleProducer {
+
+	public static void main(String[] args) {
+
+		Properties props = new Properties();
+		props.put("bootstrap.servers", Configuration.BOOTSTRAP_SERVERS);
+		props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+		props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+		props.put("acks", "all"); 
+		props.put("retries", "3");
+		props.put("max.in.flight.requests.per.connection", "1");
+		Producer<String, String> producer = new KafkaProducer<String, String>(props);
+
+		ProducerRecord<String, String> producerRecord = new ProducerRecord<String, String>("demo_sawitzki", null,
+				"another very simple message");
+
+		producer.send(producerRecord);
+
+		producer.close();
+	}
+
+}
