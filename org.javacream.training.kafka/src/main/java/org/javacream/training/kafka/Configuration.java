34c2fa0b91ec2e94f183aa97d19a23c2ac35cc82
Rainer Sawitzki <training@rainer-sawitzki.de>
2021-02-05 07:06:14 +0100

    add spring boot project

--- org.javacream.training.kafka/src/main/java/org/javacream/training/kafka/Configuration.java
+++
@@ -1,7 +0,0 @@
-package org.javacream.training.kafka;
-
-public class Configuration {
-
-	public static final String HOST = "localhost";
-	public static final String BOOTSTRAP_SERVERS = HOST + ":9092";
-}
