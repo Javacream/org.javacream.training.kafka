786aab50faa1676ab78768885546abd123be1326
Rainer Sawitzki <training@rainer-sawitzki.de>
2021-03-04 21:42:45 +0100

    add docker and sample projects

---
+++ projects/org.javacream.training.kafka.spring/src/main/java/org/javacream/training/kafka/spring/log/LogPartitioner.java
@@ -0,0 +1,47 @@
+package org.javacream.training.kafka.spring.log;
+
+import java.util.List;
+import java.util.Map;
+
+import org.apache.kafka.clients.producer.Partitioner;
+import org.apache.kafka.common.Cluster;
+import org.apache.kafka.common.PartitionInfo;
+
+public class LogPartitioner implements Partitioner {
+
+    public int partition(final String topic,
+                         final Object objectKey,
+                         final byte[] keyBytes,
+                         final Object value,
+                         final byte[] valueBytes,
+                         final Cluster cluster) {
+
+        final List<PartitionInfo> partitionInfoList =
+                cluster.availablePartitionsForTopic(topic);
+        final int partitionSize = partitionInfoList.size();
+        final int criticalPartition = partitionSize - 1;
+        final int partitionCount = partitionSize- 1;
+
+        final String key = ((LogMessage) objectKey).getLevel();
+        int count;
+        if (key.contains("CRITICAL")) { 
+            count = criticalPartition;
+        } else {
+            count = Math.abs(key.hashCode()) % partitionCount;
+        }
+        return count;
+
+    }
+
+	public void configure(Map<String, ?> configs) {
+		// nothing needed
+		
+	}
+
+	public void close() {
+		// nothing needed
+		
+	}
+
+
+}
