2e2ea2eb5575cb5b44ad7364fc43cb9c2b85c76f
Rainer Sawitzki <training@rainer-sawitzki.de>
2022-02-15 10:50:43 +0100

    add projects

---
+++ projects/org.javacream.training.kafka.spring/src/test/java/org/javacream/training/kafka/spring/ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.javacream.training.kafka.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
