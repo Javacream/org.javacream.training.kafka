648c00d10346bc4bd2e237cdce6f2bf2ad946f0a
Rainer Sawitzki <training@rainer-sawitzki.de>
2023-05-05 08:24:21 +0200

    add java projects

---
+++ projects/org.javacream.training.kafka.spring/src/main/java/org/javacream/training/kafka/spring/log/LogConsumer.java
@@ -0,0 +1,17 @@
+package org.javacream.training.kafka.spring.log;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.TopicPartition;
+import org.springframework.stereotype.Component;
+
+@Component
+public class LogConsumer {
+
+	@KafkaListener(id = "log_sawitzki", topicPartitions = {@TopicPartition(topic = "logs_sawitzki", partitions = {"0", "1"})})
+	public void listen1(List<LogMessage> logMessages) throws IOException {
+			logMessages.forEach(message -> System.out.println(message));
+	}
+}
